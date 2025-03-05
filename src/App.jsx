58348a6f8fba6da519edb026b3bb195a8a15d85e
Che Meng Her <che@chemac.local>
2025-03-05 16:31:14 -0700

    refactor(header/active link): removed the header from the index.html file into the main app.jsx file on top of all of the routes found within the app itself. also added in the underlining of the current page we are on

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import Home from "./components/home.component";
 import AboutMe from "./components/about_me.component";
 
@@ -9,13 +15,10 @@ function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-        </Routes>
-        <Routes>
           <Route path="/projects" element={<ComingSoon />}></Route>
-        </Routes>
-        <Routes>
           <Route path="/about_me" element={<AboutMe />}></Route>
         </Routes>
       </Router>
@@ -25,6 +28,49 @@ function App() {
 
 export default App;
 
+const Header = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const links = document.querySelectorAll(".nav-link");
+
+    // sanity check here to make sure that we remove all current page classes on links
+    links.forEach((link) => link.classList.remove("current_page"));
+
+    // Let's look for the current link
+    const active_link = Array.from(links).find(
+      (link) => link.getAttribute("href") === location.pathname,
+    );
+
+    if (active_link) {
+      active_link.classList.add("current_page")
+    }
+  }, [location.pathname]);
+
+  return (
+    <header className="header">
+      <div className="header__content">
+        <div>
+          <Link to="/" className="logo">
+            Che.
+          </Link>
+        </div>
+        <div className="nav-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/projects" className="nav-link">
+            Projects
+          </Link>
+          <Link to="/about_me" className="nav-link">
+            About Me
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
 const ComingSoon = () => {
   return (
     <div
@@ -33,7 +79,7 @@ const ComingSoon = () => {
         height: "30vh",
         justifyContent: "center",
         alignItems: "center",
-        color: "var(--auxilary_color)"
+        color: "var(--auxilary_color)",
       }}
     >
       <div>Projects Coming Soon!</div>
