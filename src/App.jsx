7832b0a22a8ee332f6dd92a677b54101958d2c8c
Che Meng Her <che@chemac.local>
2025-03-04 23:15:32 -0700

    feat(projects): added a "coming soon" message to the projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home.component";
 import AboutMe from "./components/about_me.component";
@@ -12,6 +13,9 @@ function App() {
           <Route path="/" element={<Home />}></Route>
         </Routes>
         <Routes>
+          <Route path="/projects" element={<ComingSoon />}></Route>
+        </Routes>
+        <Routes>
           <Route path="/about_me" element={<AboutMe />}></Route>
         </Routes>
       </Router>
@@ -20,3 +24,19 @@ function App() {
 }
 
 export default App;
+
+const ComingSoon = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "30vh",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "var(--auxilary_color)"
+      }}
+    >
+      <div>Projects Coming Soon!</div>
+    </div>
+  );
+};
