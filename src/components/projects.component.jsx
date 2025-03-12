70ffe241741c846f271afb47eef8c0e8402e6b1a
Che Meng Her <che@chemac.local>
2025-03-12 12:59:54 -0600

    fix(darken font colors): darken font colors to make the site more readable

--- src/components/projects.component.jsx
+++ src/components/projects.component.jsx
@@ -23,11 +23,43 @@ const projects = [
       </>,
     ],
   },
+  // {
+  //   project_name: "Auto Data Science",
+  //   url: "/images/projects/portfolio.png",
+  //   project_description: [
+  //     "Completely Built from the Ground Up",
+  //     "Built with React",
+  //     "Deployed and Hosted on Vercel",
+  //   ],
+  // },
+  // {
+  //   project_name: "Portfolio Website",
+  //   url: "/images/projects/portfolio.png",
+  //   project_description: [
+  //     "Completely Built from the Ground Up",
+  //     "Built with React",
+  //     "Deployed and Hosted on Vercel",
+  //     <>
+  //       Icons provided by{" "}
+  //       <a href="https://icons8.com" target="_blank" rel="noopener noreferrer">
+  //         Icons8
+  //       </a>{" "}
+  //       and{" "}
+  //       <a
+  //         href="https://www.flaticon.com"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Flaticon
+  //       </a>
+  //     </>,
+  //   ],
+  // },
 ];
 
 const Projects = () => {
   return (
-    <div style={{display: "flex", justifyContent: "center"}}>
+    <div style={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
       {projects.map((project, index) => (
         <div className="project_card" key={index}>
           <img src={project.url} alt={project.project_name} />
